38e10228b2bde2fa3b9d677beaf15ad5beeab56a
Kelly Ye <kellyye@berkeley.edu>
2024-12-06 03:03:50 -0700

    established and confirmed connection between backend and frontend

--- client/src/elements/JudgeProfile.jsx
+++ client/src/elements/JudgeProfile.jsx
@@ -1,51 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import '../App.css';
 import '../JudgeProfile.css';
 
 function JudgeProfile() {
+    const [judgeData, setJudgeData] = useState([]); //to store fetched data
+    useEffect(()=>{
+        axios.get('http://localhost:4000/api/judge/').then((res) => {
+            console.log("successfully posted get request");
+            setJudgeData(res.data);
+        })
+        .catch((err)=>console.log(err))
+    }, []);
+
     return (
         <div class="main">
             <div>
-            <h1 style={{margin: 0}}> Judge Name </h1>
-            <h4 style={{margin: 0}}> The College Preparatory School</h4>
+                {judgeData && judgeData.length > 0 ? (
+                    judgeData.map((judge, index) => (
+                        <div>
+                            <h1> {judge.name} </h1>
+                            <h4> {judge.affiliation}</h4>
+
+                            <div class="container-spacing container">
+                                <h3 style={{margin: 0}}> Stats </h3>
+
+                                <div class="h-between" style={{marginTop: 8, marginBottom: 8}}>
+                                    <div class="stat-instance">
+                                        <h5> Years Judging </h5>
+                                        <p class="stat-text"> -- </p>
+                                    </div>
+
+                                    <div class="stat-instance">
+                                        <h5> Years in Debate </h5>
+                                        <p class="stat-text"> -- </p>
+                                    </div>
+
+                                    <div class="stat-instance">
+                                        <h5> Speaker Pt Avg </h5>
+                                        <p class="stat-text"> -- </p>
+                                    </div>
+                                </div>
+
+                                <div style={{marginTop: 8, marginBottom: 8}}>
+                                <div class="stat-instance">
+                                        <h5> 24-25 Topic Round Stats </h5>
+                                        <p class="stat-text"> 5 years </p>
+                                        <p class="stat-text"> 5 years </p>
+                                        <p class="stat-text"> 5 years </p>
+                                        <p class="stat-text"> 5 years </p>
+                                        <p class="stat-text"> 5 years </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="history container container-spacing">
+                                <h3 style={{margin: 0}}> Judge Information </h3>
+
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <div>
+                        <p> failed to load resource </p>
+                    </div>
+
+                )}
             </div>
 
-            <div class="container-spacing container">
-                <h3 style={{margin: 0}}> Stats </h3>
-
-                <div class="h-between" style={{marginTop: 8, marginBottom: 8}}>
-                    <div class="stat-instance">
-                        <h5> Years Judging </h5>
-                        <p class="stat-text"> 5 years </p>
-                    </div>
-
-                    <div class="stat-instance">
-                        <h5> Years in Debate </h5>
-                        <p class="stat-text"> 9 years </p>
-                    </div>
-
-                    <div class="stat-instance">
-                        <h5> Speaker Pt Avg </h5>
-                        <p class="stat-text"> 28.8 </p>
-                    </div>
-                </div>
-
-                <div style={{marginTop: 8, marginBottom: 8}}>
-                <div class="stat-instance">
-                        <h5> 24-25 Topic Round Stats </h5>
-                        <p class="stat-text"> 5 years </p>
-                        <p class="stat-text"> 5 years </p>
-                        <p class="stat-text"> 5 years </p>
-                        <p class="stat-text"> 5 years </p>
-                        <p class="stat-text"> 5 years </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="history container container-spacing">
-                <h3 style={{margin: 0}}> Judging History </h3>
-
-            </div>
         </div>
     )
 }
--- server/server.js
+++ server/server.js
@@ -11,21 +11,43 @@ const app = express()
 app.use(express.static(path.join(__dirname, "public")))
 
 // Cross-Origin Resource Sharing to manage and control web security
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 // Parse JSON data from HTTP requests to process data sent from the client
 app.use(express.json())
 
 // Configure the application port to specify where server listens for incoming requests
-const port = 5000
+const port = 4000
 
 // Establish connection to MySQL database to ensure it interacts effectively
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "H556Fz62",
-    database: "prefbuddy"
+    password: '',
+    database: "pref-buddy",
+    port: 3306
 })
+
+// app.get('/', (req, res) => {
+//     res.send("GET request called to root");
+// })
+
+
+app.get('/api/judge', (req, res) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error('Database error: ', err);
+            return res.status(500).json({error: 'Failed to fetch data'});
+        }
+        console.log("Query results: ", result);
+        return res.json(result);
+    });
+});
 
 // Start server to respond to incoming requests
 app.listen(port, ()=>{
