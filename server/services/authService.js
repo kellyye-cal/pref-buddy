97adeccc19946b650730bf540d3b0a3455dcd142
Kelly Ye <kellyye@berkeley.edu>
2025-03-06 01:24:45 -0800

    fixed authservice db connection

--- server/services/authService.js
+++ server/services/authService.js
@@ -4,12 +4,19 @@ const jwt = require('jsonwebtoken')
 const verifyJWT = require('../../middleware/verifyJWT')
 // require('dotenv').config({path: '../../.env'});
 
+// const db = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: '',
+//     database: "pref-buddy",
+//     port: 3306
+// })
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: '',
-    database: "pref-buddy",
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 })
 
 const registerUser = async({email, fname, lname, pwd}) => {
